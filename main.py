437df291ff4b49da384fc04866d3e8de54db11fe
Nikhil Gaikwad <162660405+Lthecoders@users.noreply.github.com>
2024-04-27 00:52:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# print("wow")
+"------------------day 4 challenge---------------------"
+# print(
+#     "welcome to the adventure simulatore. I am gooing to ask you a bunch of questions and then create an epic story with you as the star!"
+# )
+# print()
+# player_name = input("enter your gamming name ")
+# player_game = input("enter the game of your favorite ")
+# save = input("bad peoples")
+# end = input("do you servied the game or did you died'😢 ")
+# what_end = input(
+#     "what do you think, is still you achivements in reconized in the game? ")
+# print()
+# print("there was a great gamer named", "\033[31m", player_name, "\033[0m",
+#       "he loved to play", player_game, "he saved all of them from", save,
+#       "but at last he", end, "but his achievements", what_end)
+
+# print("Uh, oh, you've been given a", "\033[35m", "warning", "\033[0m",
+#       "for being a bad, bad person.")
+
+
+
+
+
+print("\033[31m","you are a devil","\033[32m","\n i am a god")
+
+
